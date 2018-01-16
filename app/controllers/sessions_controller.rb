3e60a88e8a66d70b866573ef90a600770f208bb8
erikbenton <erik.benton@hotmail.com>
2018-01-16 13:29:25 -0500

    adding basic authentication when logging in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		# Log the user in and redirect to show user's show
+  	else
+  		# Create an error message
+  	render "new"
+  end
+
+  def destroy
+  	
+  end
 end
