ffc357a4913cf700652e1b7e5e4c395a2bf0c7bb
erikbenton <erik.benton@hotmail.com>
2018-01-23 15:28:00 -0500

    adding user index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:erik)
+  end
+
   test 'layout links' do
   	get root_path
   	assert_template 'static_pages/home'
@@ -12,5 +17,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select 'title', full_title("Contact")
     get signup_path
     assert_select 'title', full_title("Sign up")
+    get help_path
+    assert_select 'title', full_title("Help")
+    get users_path
+    assert_redirected_to login_url
+    get login_path
+    assert_select "title", full_title("Log in")
+    log_in_as(@user)
+    get users_path
+    assert_select "title", full_title("All users")
   end
 end
