6a4f0a908d8967814d04e51ffc6a1530b5b41d29
erikbenton <erik.benton@hotmail.com>
2018-01-24 12:12:00 -0500

    fixing production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'gentle-mountain-38405.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_setting = {
-    :address              => 'smtp.sendgrid.net',
-    :port                 => '587',
-    :authentication       => :plain,
-    :user_name            => ENV['SENDGRID_USERNAME'],
-    :password             => ENV['SENDGRID_PASSWORD'],
-    :domain               => 'heroku.com',
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
